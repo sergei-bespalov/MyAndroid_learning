5859d3add845a7c99e3191a3d379e72b5847594e
sergei-bespalov <sergei.bespalov@gmail.com>
2014-12-24 08:37:57 +0300

    MultiTouch

--- Dots2/app/src/main/java/dots/sergei/bespalov/dots/TrackingTouchListener.java
+++ Dots2/app/src/main/java/dots/sergei/bespalov/dots/TrackingTouchListener.java
@@ -1,22 +1,73 @@
 package dots.sergei.bespalov.dots;
 
 import android.graphics.Color;
+import android.support.v4.view.MotionEventCompat;
 import android.view.MotionEvent;
 import android.view.View;
+
+import java.util.LinkedList;
 
 /**
  * Created by Sergei on 21.12.2014.
  */
 public class TrackingTouchListener implements View.OnTouchListener {
     private final Dots dots;
+    private volatile LinkedList<Integer> tracks = new LinkedList<Integer>();
 
-    TrackingTouchListener(Dots dots){
+    TrackingTouchListener(Dots dots) {
         this.dots = dots;
     }
 
     @Override
+    /**
+     * multi touch
+     */
     public boolean onTouch(View v, MotionEvent event) {
-        switch (event.getAction()){
+        int action = event.getActionMasked();
+        int idx;
+        int n;
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_POINTER_DOWN:
+                idx = MotionEventCompat.getActionIndex(event);
+                tracks.add(event.getPointerId(idx));
+                break;
+            case MotionEvent.ACTION_POINTER_UP:
+                idx = MotionEventCompat.getActionIndex(event);
+                tracks.remove(event.getPointerId(idx));
+                break;
+            case MotionEvent.ACTION_MOVE:
+                n = event.getHistorySize();
+                for (Integer i : tracks) {
+                    idx = event.findPointerIndex(i.intValue());
+                    for (int j = 0; j < n; j++) {
+                        addDot(
+                                dots,
+                                event.getHistoricalX(idx, j),
+                                event.getHistoricalY(idx, j),
+                                event.getHistoricalPressure(idx, j),
+                                event.getHistoricalSize(idx, j)
+                        );
+                    }
+                }
+                break;
+            default:
+                return false;
+        }
+        for (Integer i: tracks){
+            idx = event.findPointerIndex(i.intValue());
+            addDot(
+                    dots,
+                    event.getX(idx),
+                    event.getY(idx),
+                    event.getPressure(idx),
+                    event.getSize(idx)
+            );
+        }
+        return true;
+    }
+    /*public boolean onTouch(View v, MotionEvent event) {
+         switch (event.getAction()){
             case  MotionEvent.ACTION_DOWN:
                 break;
             case  MotionEvent.ACTION_MOVE:
@@ -33,22 +84,31 @@ public class TrackingTouchListener implements View.OnTouchListener {
             default:
                 return false;
         }
-        /*addDot(
-                dots,
-                event.getX(),
-                event.getY(),
-                event.getPressure(),
-                event.getSize()
-        );*/
-        return true;
-    }
+        //addDot(
+        //        dots,
+        //        event.getX(),
+        //        event.getY(),
+        //        event.getPressure(),
+        //       event.getSize()
+        //);
 
-    private void addDot(Dots dots, float x, float y, float p, float s){
+    }*/
+
+    /**
+     * add dot to dots
+     *
+     * @param dots
+     * @param x    x coordinate
+     * @param y    y coordinate
+     * @param p    pressure
+     * @param s    size
+     */
+    private void addDot(Dots dots, float x, float y, float p, float s) {
         dots.addDot(
                 x,
                 y,
                 Color.CYAN,
-                (int)((p*s*Dot.DIAMETER) + 1)
+                (int) ((p * s * Dot.DIAMETER) + 1)
         );
     }
 }
